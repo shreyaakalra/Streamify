113779216034aece6b61e2d0ce5888227ad17986
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-03-09 03:03:47 +0530

    nextauth route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,9 @@
+// WHY DO WE EVEN HAVE THIS FILE?????
+
+// If we didn't have NextAuth, you wouldn't just have to write the database check. You would have to hand-write the cryptography to generate a token, hand-write the code to inject it into the browser's cookie storage, and hand-write the middleware to check that cookie on every single page refresh. It would be hundreds of lines of complex security code.
+
+// [...nextauth]/route.ts is the engine that runs your entire app's security. It logs people in, it creates the cookies, and it remembers who the user is as they click around your website.
+
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@/lib/prisma";
@@ -16,23 +22,30 @@ const handler = NextAuth({
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials) {
+        // check if user entered both email and password
         if (!credentials?.email || !credentials?.password) {
           throw new Error("Invalid credentials");
         }
 
+        // prisma asks mongodb if they have anyone in the vault with this exact email
         const user = await prisma.user.findUnique({
-          where: { email: credentials.email }
+          where: { 
+            email: credentials.email 
+          }
         });
 
+        // if mongodb has never heard of them we return invalid credentials
         if (!user || !user?.hashedPassword) {
           throw new Error("Invalid credentials");
         }
 
+        // since we cannot unshred the password we take the password just typed run it through the exact same mathematical shredder and see if it matches perfectly
         const isCorrectPassword = await bcrypt.compare(
           credentials.password,
           user.hashedPassword
         );
 
+        // if correct password return user else return invalid credentials
         if (!isCorrectPassword) {
           throw new Error("Invalid credentials");
         }
@@ -42,9 +55,9 @@ const handler = NextAuth({
     })
   ],
   session: {
-    strategy: "jwt"
+    strategy: "jwt" // tells nextauth to hand the user a JWT instead of saving their session in db.
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET, // this is the invisible ink used to sign that wristband so hackers can't forge it
 });
 
 export { handler as GET, handler as POST };
